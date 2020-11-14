ac6b14cc411ab1d28f3375479d2bbe26a40cf1b7
QING <2298722518@qq.com>
2020-11-14 20:27:14 +0800

    编程实现：两个int（32位）整数m和n的二进制表达中，有多少个位(bit)不同？ 输入例子：1999 2299 输出例子:7

--- 2020-11-14/test.c
+++ 2020-11-14/test.c
@@ -2,43 +2,27 @@
 #include<Windows.h>
 #include<string.h>
 #pragma warning(disable:4996)
-void reverse_str(char* begin, char* end)
+int Compare(int a, int b)
 {
-	while (begin < end)
+	int c = a^b;
+	int i = 0;
+	int count = 0;
+	for (int i = 0; i < 32; i++)
 	{
-		char tmp = *begin;
-		*begin = *end;
-		*end = tmp;
-		begin++;
-		end--;
+		if (((c >> i) & 1) == 1)
+		{
+			count++;
+		}
 	}
+	return count;
 }
 int main()
 {
-	//字符串接收
-	char str[100] = { 0 };
-	//一定要预留\0的位置
-	fgets(str, sizeof(str)-1, stdin);
-	str[strlen(str)-1] = '\0';//输入i like beijing.\n
-	char *begin = str;
-	char *end = str;
-	//每个单词逆置
-	while (*end != '\0')
-	{
-		while (*end != '\0' && *end != ' ')
-		{
-			end++;
-		}
-		reverse_str(begin, end - 1);
-		if (*end != '\0')
-		{
-			end++;
-		}
-		begin = end;
-	}
-	//整个字符串逆置
-	reverse_str(str, str + strlen(str)-1);
-	printf("%s\n", str);
+	int a = 1999;
+	int b = 2299;
+	printf("%d\n", Compare(a, b));
 	system("pause");
 	return 0;
 }
+
+
