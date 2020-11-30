951f277a3dc42389d18b2d7dd6cb2c306d800e32
QING <2298722518@qq.com>
2020-11-30 22:56:31 +0800

    数组交换

---
+++ 2020-11-30/test.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<Windows.h>
+#include<string.h>
+void Print(int arr[], int len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		printf("arr[%d]= %2d \n", i, arr[i]);
+	}
+}
+void SWAP(int arr1[], int arr2[], int len)
+{
+	int temp = 0;
+	for (int i = 0; i < len; i++)
+	{
+		temp = arr1[i];
+		arr1[i] = arr2[i];
+		arr2[i] = temp;
+	}
+}
+
+int main()
+{
+	int arr1[] = { 1, 21, 3, 4, 35, 6, 9 };
+	int arr2[] = { 4, 5, 46, 7, 28, 9, 7 };
+	int len = sizeof(arr1) / sizeof(arr1[0]);
+	printf("交换前arr1:");
+	Print(arr1, len);
+	printf("交换前arr2:");
+	Print(arr2, len);
+	SWAP(arr1, arr2, len);
+	printf("交换后arr1:");
+	Print(arr1, len);
+	printf("交换后arr2:");
+	Print(arr2, len);
+	system("pause");
+	return 0;
+}
