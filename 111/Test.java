a2dae2eb8936f718136776f4c14b1ebf778d8902
QING <2298722518@qq.com>
2020-12-29 21:43:08 +0800

    简单练习

---
+++ 111/Test.java
@@ -0,0 +1,24 @@
+public class Test {
+    /*public static void main1(String[] args) {
+        int maxvalue=Integer.MAX_VALUE;
+        System.out.println(maxvalue+1);
+    }*/
+    /*public static void main(String[] args) {
+        String a="hello";
+        String b="world";
+        System.out.println(a+b);
+    }*/
+   /* public static void main(String[] args) {
+       int num=10;
+       String str1=num+"";
+       String str2=String.valueOf(num);
+        System.out.println(str1);
+        System.out.println(str2);
+    }*/
+    public static void main(String[] args) {
+        int a=10;
+        long b=20;
+        a+=b;
+        System.out.println(a);
+    }
+}
