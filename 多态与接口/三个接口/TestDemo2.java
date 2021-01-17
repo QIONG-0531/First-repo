5e060db9dd0ae960b23c01bd74930259c191419c
QING <2298722518@qq.com>
2021-01-17 18:40:47 +0800

    多态与接口

---
+++ 多态与接口/三个接口/TestDemo2.java
@@ -0,0 +1,41 @@
+package 三个接口;
+
+import java.util.Arrays;
+
+//Comparator比较器一般用在类外，用的是compare方法
+class Student2{
+    public String name;
+    public int score;
+
+    public Student2(String name,int score) {
+        this.name = name;
+        this.score=score;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+}
+public class TestDemo2 {
+    public static void main(String[] args) {
+        Student2[] students = new Student2[3];
+        students[0] = new Student2("bit",89);
+        students[1] = new Student2("abc",19);
+        students[2] = new Student2("htf",59);
+        System.out.println(Arrays.toString(students));
+        System.out.println("===============根据分数排序==============");
+        ScoreComparator scoreComparator = new ScoreComparator();
+        Arrays.sort(students,scoreComparator);
+        System.out.println(Arrays.toString(students));
+        System.out.println("===============根据姓名进行排序==============");
+        NameComparator nameComparator = new NameComparator();
+        Arrays.sort(students,nameComparator);
+        System.out.println(Arrays.toString(students));
+    }
+
+
+}
