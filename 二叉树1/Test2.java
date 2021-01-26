1f74cb9fed34415a47cf7aba87013b2c9d20a017
QING <2298722518@qq.com>
2021-01-26 17:35:55 +0800

    练习

---
+++ 二叉树1/Test2.java
@@ -0,0 +1,33 @@
+/*
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    //合并二叉树
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        }
+        if (t2 == null) {
+            return t1;
+        }
+        TreeNode tree = new TreeNode(t1.val + t2.val);
+        tree.left = mergeTrees(t1.left, t2.left);
+        tree.right = mergeTrees(t1.right, t2.right);
+        return tree;
+    }
+}*/
