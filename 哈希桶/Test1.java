797c249ddbff84594255ca6294eea3b484f1f653
QING <2298722518@qq.com>
2021-02-06 20:05:17 +0800

    宝石与石头

---
+++ 哈希桶/Test1.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+public class Test1 {
+    //宝石与石头
+    public int numJewelsInStones(String jewels, String stones) {
+        HashSet<Character> set=new HashSet<>();
+        for(int i=0;i<jewels.length();i++){
+            set.add(jewels.charAt(i));
+        }
+        int count=0;
+        for(int i=0;i<stones.length();i++){
+            if(set.contains(stones.charAt(i))){
+                count++;
+            }
+        }
+        return count;
+    }
+}
