18f8f29d2dac5131331c2d936560096936820837
QING <2298722518@qq.com>
2021-02-23 14:24:25 +0800

    练习

---
+++ 单链表练习题/Test5.java
@@ -0,0 +1,19 @@
+public class Test5 {
+    //判断链表是否有环
+    public boolean hasCycle(ListNode head) {
+        if(head==null){
+            return false;
+        }
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
